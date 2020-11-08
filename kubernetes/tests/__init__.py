51c21cdb96da62e085b26c4179309415202175c9
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2020-11-08 21:18:32 +0100

    🏗 (tests): add __init__.py in tests/ for vscode discovery
    ... but this should not be necessary - see file content for more

---
+++ kubernetes/tests/__init__.py
@@ -0,0 +1,8 @@
+# THIS FILE SHOULDN'T EXIST
+# It's due to a very poor handling of PYTHONPATH for discovery module by VScode
+# For more, see:
+# - https://github.com/microsoft/vscode-python/issues/8678
+# - https://stackoverflow.com/questions/10253826/path-issue-with-pytest-importerror-no-module-named-yadayadayada/50610630#50610630
+# - https://code.visualstudio.com/docs/python/environments#_use-of-the-pythonpath-variable
+# ... and many other reports and ramblings here and there.
+
