51a6e6c1141f4fc9786834105bc81f13199bed34
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2020-11-09 02:38:02 +0100

    🎨 (tests): apply black formatting on tests/__init__.py

--- kubernetes/tests/__init__.py
+++ kubernetes/tests/__init__.py
@@ -5,4 +5,3 @@
 # - https://stackoverflow.com/questions/10253826/path-issue-with-pytest-importerror-no-module-named-yadayadayada/50610630#50610630
 # - https://code.visualstudio.com/docs/python/environments#_use-of-the-pythonpath-variable
 # ... and many other reports and ramblings here and there.
-
